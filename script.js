cbaf4a3b2570d98d4f521d27897abb5305ffb616
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-02-03 21:20:48 +0500

    Team Quvonchbek

---
+++ script.js
@@ -0,0 +1,43 @@
+function btn1() {
+    let TQQ = ["Tosh", "Qaychi", "Qog'oz"];
+    let Random = Math.floor(Math.random() * TQQ.length);
+    if (TQQ[Random] == TQQ[0]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[0]} durang`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[0]}ni tanladingiz Biz ham ${TQQ[0]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Durang'} ${document.getElementById('bg1').style.display = 'block'} ${document.getElementById('bg3').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[1]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[0]} yutdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[0]}ni tanladingiz Biz esa ${TQQ[1]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutdingiz'} ${document.getElementById('bg2').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg3').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[2]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[0]} yutqazdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[0]}ni tanladingiz Biz esa ${TQQ[2]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    }
+}
+function btn2() {
+    let TQQ = ["Tosh", "Qaychi", "Qog'oz"];
+    let Random = Math.floor(Math.random() * TQQ.length);
+    if (TQQ[Random] == TQQ[1]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[1]} durang`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[1]}ni tanladingiz Biz ham ${TQQ[1]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Durang'} ${document.getElementById('bg1').style.display = 'block'} ${document.getElementById('bg3').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[2]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[1]} yutdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[1]}ni tanladingiz Biz esa ${TQQ[2]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutdingiz'} ${document.getElementById('bg2').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg3').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[0]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[1]} yutqazdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[1]}ni tanladingiz Biz esa ${TQQ[0]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    }
+}
+function btn3() {
+    let TQQ = ["Tosh", "Qaychi", "Qog'oz"];
+    let Random = Math.floor(Math.random() * TQQ.length);
+    if (TQQ[Random] == TQQ[2]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[2]} durang`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[2]}ni tanladingiz Biz ham ${TQQ[2]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Durang'} ${document.getElementById('bg1').style.display = 'block'} ${document.getElementById('bg3').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[0]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[2]} yutdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[2]}ni tanladingiz Biz esa ${TQQ[0]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutdingiz'} ${document.getElementById('bg2').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg3').style.display = 'none'}`
+    } else if (TQQ[Random] == TQQ[1]) {
+        console.log(`biz:${TQQ[Random]} siz:${TQQ[2]} yutqazdingiz`);
+        `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[2]}ni tanladingiz Biz esa ${TQQ[1]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
+    }
+}
+// Good Luck
