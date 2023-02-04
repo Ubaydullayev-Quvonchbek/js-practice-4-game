65a0b312c0dcf5941175fcc2c5352c3108ebeefa
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-02-04 13:48:06 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function btn1() {
         console.log(`biz:${TQQ[Random]} siz:${TQQ[0]} yutqazdingiz`);
         `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[0]}ni tanladingiz Biz esa ${TQQ[2]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
     }
+    document.getElementById('btn1').style.marginTop = '.3%';
+    document.getElementById('btn2').style.marginTop = '.3%';
+    document.getElementById('btn3').style.marginTop = '.3%';
 }
 function btn2() {
     let TQQ = ["Tosh", "Qaychi", "Qog'oz"];
@@ -25,6 +28,9 @@ function btn2() {
         console.log(`biz:${TQQ[Random]} siz:${TQQ[1]} yutqazdingiz`);
         `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[1]}ni tanladingiz Biz esa ${TQQ[0]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
     }
+    document.getElementById('btn1').style.marginTop = '.3%';
+    document.getElementById('btn2').style.marginTop = '.3%';
+    document.getElementById('btn3').style.marginTop = '.3%';
 }
 function btn3() {
     let TQQ = ["Tosh", "Qaychi", "Qog'oz"];
@@ -39,5 +45,8 @@ function btn3() {
         console.log(`biz:${TQQ[Random]} siz:${TQQ[2]} yutqazdingiz`);
         `${document.getElementById('heading1').innerHTML = `Siz ${TQQ[2]}ni tanladingiz Biz esa ${TQQ[1]}ni tanladik`} ${document.getElementById('heading2').innerHTML = 'Siz yutqazdingiz'} ${document.getElementById('bg3').style.display = 'block'} ${document.getElementById('bg1').style.display = 'none'} ${document.getElementById('bg2').style.display = 'none'}`
     }
+    document.getElementById('btn1').style.marginTop = '.3%';
+    document.getElementById('btn2').style.marginTop = '.3%';
+    document.getElementById('btn3').style.marginTop = '.3%';
 }
 // Good Luck
